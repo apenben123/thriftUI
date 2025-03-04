bcc53c25f14f976afacbac20366787e59aec0c23
apenben123 <48937379+apenben123@users.noreply.github.com>
2025-03-04 11:16:29 +0800

    Update make.bat

--- make.bat
+++ make.bat
@@ -2,7 +2,10 @@
 chcp 65001 > nul
 
 echo [build] start
-cd  Z:\test\python_test\thrift_tool
+
+REM 获取当前脚本所在目录
+set "current_dir=%~dp0"
+cd /d "%current_dir%"
 echo [build] path: %cd%
 
 REM 获取当前目录名
@@ -14,26 +17,32 @@ python -m venv _env
 echo [build] create venv end
 
 echo [build] enter venv 
-echo [build] path: %cd%
 call _env\Scripts\activate.bat
-echo [build] create done 
-
-REM  check venv
 if %errorlevel% neq 0 (
-    echo [build] Failed to create virtual environment.
+    echo [build] Failed to activate virtual environment.
     pause
     exit /b 1
 )
+echo [build] create done 
 
 echo [build] pip requirements start
 pip -V
 pip install -r requirements.txt
+if %errorlevel% neq 0 (
+    echo [build] Failed to install dependencies.
+    pause
+    exit /b 1
+)
 pip list
 echo [build] pip requirements end
 
-
 echo [build] build start
 pyinstaller -F --clean --distpath=./bin --noconsole --name="%current_dir_name%.exe" ./main.py
+if %errorlevel% neq 0 (
+    echo [build] Failed to build executable.
+    pause
+    exit /b 1
+)
 echo [build] build done
 
 echo [build] quit venv
